d9e80a4bf877694293d1111de5c2d2048e8091c7
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-05-26 23:23:20 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,4 +6,14 @@ def array_with_two_elements
   dog_breed_array = ["beagle", "golden retriever"]
 end
 
-def first_element
+def first_element(array)
+  array[0]
+end  
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[-1]
+end
